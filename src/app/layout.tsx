91e05432211d815a844d0883ac0cc1a073c55166
Ruben Marcus <ruben@rubenmarcus.dev>
2025-02-10 22:38:59 +0000

    remove package lock

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SuiProvider } from "./providers/SuiProvider";
+import { Metadata } from "next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -11,6 +12,31 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+export const metadata: Metadata = {
+  title: 'Sui AI Image Generator & NFT Minter',
+  openGraph: {
+    title: 'Sui AI Image Generator & NFT Minter',
+    description: 'AI Project',
+    images: [
+      {
+        type: 'image/png',
+        url: 'https://www.kriptofoni.com/wp-content/uploads/2024/11/sui-3.webp',
+        width: '1200',
+        height: '630',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Sui AI Image Generator & NFT Minter',
+    description: 'AI Project',
+    creator: 'Ruben Marcus',
+    images: 'https://www.kriptofoni.com/wp-content/uploads/2024/11/sui-3.webp',
+  },
+  description: 'AI Project',
+};
+
 
 export default function RootLayout({
   children,
